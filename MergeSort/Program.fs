7740074b6f2fbe850b992e0e7c66f4374703869c
Sebastian Hofer <sebhofer@gmail.com>
2023-11-26 15:21:51 +0100

    rename unit tests

--- MergeSort/Program.fs
+++ MergeSort/Program.fs
@@ -86,7 +86,7 @@ module Program =
 
     [<EntryPoint>]
     let main _ =
-
+        System.Collections.Generic.Dictionary()
         for A in [ [| 1; 0 |]; [| 2; 1; 0 |]; [| 3; 2; 1; 0 |]; [| 10..-1..0 |] ] do
             let B = Array.copy A
             BottomUp.sortInPlace A
